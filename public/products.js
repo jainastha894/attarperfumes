5aa2b75d4f1096a34e81f0d1391cc597634254dc
Akshayj2005 <bbpsgrakshayj2005@gmail.com>
2025-12-04 10:28:42 +0530

    message

---
+++ public/products.js
@@ -0,0 +1,310 @@
+let nextID = 1;
+
+window.products = [
+
+    {
+        id: nextID++,
+        name: "Perfume Bottle",
+        images: [
+            "fragnances/bottle1.png",
+            "fragnances/bottle1_side.png",
+            "fragnances/bottle1_top.png"
+        ],
+        JarsAndBottles: "Perfume Bottles",
+        description: "NF: 240",
+        size: "100ml",
+        featured: true,
+        industry: "Fragnances",
+        capacity: "",
+        height: "",
+        width: "",
+        weight: "",
+        color: "",
+        shape: "",
+        neckSize: "",
+        neckType: "",
+        material: "",
+        usage: ""
+    },
+
+    {
+        id: nextID++,
+        name: "Gustav Women",
+        images: [
+            "fragnances/bottle2.png",
+            "fragnances/bottle2_side.png"
+        ],
+        JarsAndBottles: "Perfume Bottles",
+        description: "Clear Bottle",
+        size: "100ml",
+        featured: true,
+        industry: "Fragnances",
+        capacity: "",
+        height: "",
+        width: "",
+        weight: "",
+        color: "",
+        shape: "",
+        neckSize: "",
+        neckType: "",
+        material: "",
+        usage: ""
+    },
+
+    {
+        id: nextID++,
+        name: "Colorful Car Hanging",
+        images: [
+            "fragnances/colorfulCarHanging.jpeg",
+            "fragnances/colorfulCarHanging2.jpeg"
+        ],
+        JarsAndBottles: "Ittar Bottles",
+        description: "Colorful Car hanging bottles for ittars",
+        size: "",
+        featured: false,
+        industry: "Fragnances",
+        capacity: "",
+        height: "",
+        width: "",
+        weight: "",
+        color: "",
+        shape: "",
+        neckSize: "",
+        neckType: "",
+        material: "",
+        usage: ""
+    },
+
+    {
+        id: nextID++,
+        name: "Pharma 1",
+        images: [
+            "pharma/pharma1.jpeg"
+        ],
+        JarsAndBottles: "Clear Glass Bottles",
+        description: "Clear Glass Bottles for pharma uses.",
+        size: "",
+        featured: false,
+        industry: "pharma",
+        capacity: "",
+        height: "",
+        width: "",
+        weight: "",
+        color: "",
+        shape: "",
+        neckSize: "",
+        neckType: "",
+        material: "",
+        usage: ""
+    },
+
+    {
+        id: nextID++,
+        name: "Pharma 2",
+        images: [
+            "pharma/pharma2.jpeg"
+        ],
+        JarsAndBottles: "Clear Glass Bottles",
+        description: "Clear Glass Bottles for pharma uses.",
+        size: "",
+        featured: false,
+        industry: "pharma",
+        capacity: "",
+        height: "",
+        width: "",
+        weight: "",
+        color: "",
+        shape: "",
+        neckSize: "",
+        neckType: "",
+        material: "",
+        usage: ""
+    },
+
+    {
+        id: nextID++,
+        name: "Pharma 3",
+        images: [
+            "pharma/pharma3.jpeg"
+        ],
+        JarsAndBottles: "Clear Glass Jars",
+        description: "Clear Glass Jars of 50ml for pharma uses.",
+        size: "50ml",
+        featured: true,
+        industry: "pharma"
+    },
+
+    {
+        id: nextID++,
+        name: "Floral Fragnance",
+        images: [
+            "exquisiteFragnances/Floral.jpeg"
+        ],
+        JarsAndBottles: "",
+        description: "Soft, fresh fragrances inspired by blooming flowers and natural petals.",
+        size: "",
+        featured: true,
+        industry: "",
+        ExquisiteFragnances: "Floral"
+    },
+
+    {
+        id: nextID++,
+        name: "Amber or Musk Fragnance",
+        images: [
+            "exquisiteFragnances/Amber.jpeg"
+        ],
+        JarsAndBottles: "",
+        description: "Warm, deep, and long-lasting scents with a cozy, sensual feel.",
+        size: "",
+        featured: true,
+        industry: "",
+        ExquisiteFragnances: "Amber or Musk"
+    },
+
+    {
+        id: nextID++,
+        name: "Citrus Fragnance",
+        images: [
+            "exquisiteFragnances/Citrus.jpeg"
+        ],
+        JarsAndBottles: "",
+        description: "Bright, zesty, and refreshing aromas bursting with lemon, orange, and fresh notes.",
+        size: "",
+        featured: true,
+        industry: "",
+        ExquisiteFragnances: "Citrus"
+    },
+
+    {
+        id: nextID++,
+        name: "Oud Fragnance",
+        images: [
+            "exquisiteFragnances/Oud.jpeg"
+        ],
+        JarsAndBottles: "",
+        description: "Rich, smoky, and luxurious woody scent known for its bold and exotic character.",
+        size: "",
+        featured: true,
+        industry: "",
+        ExquisiteFragnances: "Oud"
+    },
+
+    {
+        id: nextID++,
+        name: "Matka Jar",
+        images: [
+            "food/food1.jpeg"
+        ],
+        JarsAndBottles: "Clear Glass Jars",
+        description: "recyclable materials, these bottles & jars are not only dishwasher safe",
+        size: "All sizes available",
+        featured: false,
+        industry: "food"
+    },
+
+    {
+        id: nextID++,
+        name: "Round Jars",
+        images: [
+            "food/food5.png"
+        ],
+        JarsAndBottles: "Clear Glass Jars",
+        description: "Plastic/Steels caps. dishwasher-safe and recyclable glass",
+        size: "All sizes available",
+        featured: true,
+        industry: "food"
+    },
+
+    {
+        id: nextID++,
+        name: "Cylendrical Jars",
+        images: [
+            "food/food7.png"
+        ],
+        JarsAndBottles: "Clear Glass Jars",
+        description: "PLASTIC CAP, Glassfinity's Cylinderical Jars",
+        size: "All sizes available",
+        featured: false,
+        industry: "food"
+    },
+
+    {
+        id: nextID++,
+        name: "Inverted Diamond Jars",
+        images: [
+            "fragnances/bottle8.jpg"
+        ],
+        JarsAndBottles: "Clear Glass Jars",
+        description: "Glassfinity's Diamond Jars (JK4) in Cartoon of 48 Pieces",
+        size: "150ml",
+        featured: false,
+        industry: "Fragnances"
+    },
+
+    {
+        id: nextID++,
+        name: "Star Jars",
+        images: [
+            "fragnances/bottle9.jpg"
+        ],
+        JarsAndBottles: "Clear Glass Jars",
+        description: "Glassfinity's Star Jars With Crown",
+        size: "",
+        featured: false,
+        industry: "Fragnances"
+    },
+
+    {
+        id: nextID++,
+        name: "Amber Cylindrical Jars",
+        images: [
+            "fragnances/bottle10.jpg"
+        ],
+        JarsAndBottles: "Amber Glass Bottles",
+        description: "Amber Cylindrical Jars with Bulb for Spraying of in Piece Cartoon (QN 2)",
+        size: "24 ml",
+        featured: false,
+        industry: "Fragnances"
+    },
+
+    {
+        id: nextID++,
+        name: "Amber Cuboid Jars",
+        images: [
+            "fragnances/bottle11.jpg"
+        ],
+        JarsAndBottles: "Amber Glass Jars",
+        description: "Amber Cuboid Jars with Bulb for Spraying in 48 Piece Cartoon (QN 1)",
+        size: "24 ml",
+        featured: false,
+        industry: "Fragnances"
+    },
+
+    {
+        id: nextID++,
+        name: "Cylendrical Viles",
+        images: [
+            "fragnances/bottle12.jpg"
+        ],
+        JarsAndBottles: "Amber Glass Jars",
+        description: "Small Cylindrical Viles with Seperated Chamber and Colored Bottom made of Amber Glass in 100 Piece Cartoon (YNP 6ml)",
+        size: "6 ml",
+        featured: false,
+        industry: "Fragnances"
+    },
+
+    {
+        id: nextID++,
+        name: "Perfume Bottle Gold Cap",
+        images: [
+            "fragnances/bottle13.jpg"
+        ],
+        JarsAndBottles: "Clear Glass Jars",
+        description: "Elegant Perfume Bottle with Gold Cap and 4 Sides Vertical Strips in Different Sizes (BJP 2) \n3ml in 960 Piece Cartoon\n6ml in 720 Piece Cartoon\n12ml in 480 Piece Cartoon",
+        size: "All sizes available",
+        featured: false,
+        industry: "Fragnances"
+    }
+
+];
